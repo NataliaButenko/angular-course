91356929028c33ce59adb16aaffa910f05bc87c2
NataliaButenko <natkabutenko93@gmail.com>
2024-10-06 17:12:04 +0300

    Cleaned main branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { FirstDerectiveDirective } from './first-derective.directive';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent, FirstDerectiveDirective, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'my-app';
-  public isHide: boolean = true;
-  public arr: string[] = ['1', '2', '3'];
-}
+export class AppComponent {}
